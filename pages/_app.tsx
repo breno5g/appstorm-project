86e19c08c4d4839d9071520fa546964a0fe29fc3
Breno Santos <brenosantos145@gmail.com>
2022-10-25 20:58:33 -0300

    feat: add react toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,26 @@
 import type { AppProps } from 'next/app';
 import { storeWrapper } from "../store";
 import BasicLayout from '../styles/globals';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <BasicLayout>
       <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        pauseOnFocusLoss={false}
+        pauseOnHover={false}
+        theme="colored"
+      />
     </BasicLayout>
   );
 }
